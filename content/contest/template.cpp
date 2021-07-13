1e6cd59106d79f7217ad6ca18885dffd36fda807
Evirir <46616851+Evirir@users.noreply.github.com>
2021-07-12 19:30:33 -0700

    Added back some original macros

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 using namespace __gnu_pbds;
 
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
 #define mset(d,val) memset(d,val,sizeof(d))
 #define fbo find_by_order
 #define ook order_of_key
@@ -12,7 +14,7 @@ typedef pair<ll,ll> ii;
 typedef vector<ll> vi;
 template<typename T>
 using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-// gp_hash_table
+// gp_hash_table<K,V>
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0);
