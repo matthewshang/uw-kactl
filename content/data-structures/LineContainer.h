4ba3d504f20aed5d75ad54c30100450959a6ab29
Jun Xing Go <junxingg@cs.washington.edu>
2023-05-21 16:51:57 -0700

    Add min/max option for CHT

--- content/data-structures/LineContainer.h
+++ content/data-structures/LineContainer.h
@@ -1,5 +1,5 @@
 /**
- * Author: Simon Lindholm
+ * Author: Simon Lindholm edited by Evirir
  * Date: 2017-04-20
  * License: CC0
  * Source: own work
@@ -17,26 +17,29 @@ struct Line {
 };
 
 struct LineContainer : multiset<Line, less<>> {
-	// (for doubles, use inf = 1/.0, div(a,b) = a/b)
-	static const ll inf = LLONG_MAX;
-	ll div(ll a, ll b) { // floored division
-		return a / b - ((a ^ b) < 0 && a % b); }
-	bool isect(iterator x, iterator y) {
-		if (y == end()) return x->p = inf, 0;
-		if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+	const ll inf = INF; //double: inf = 1.0L
+	const bool Max = false;
+	inline ll div(ll a, ll b){
+		return a/b - ((a^b)<0 && a%b);
+	}
+	bool isect(iterator x, iterator y){
+		if(y == end()){ x->p = inf; return false; }
+		if(x->k == y->k) x->p = x->m > y->m ? inf : -inf;
 		else x->p = div(y->m - x->m, x->k - y->k);
 		return x->p >= y->p;
 	}
-	void add(ll k, ll m) {
+	void add(ll k, ll m){ // k = slope, m = y-intercept
+		if(!Max) k = -k, m = -m;
 		auto z = insert({k, m, 0}), y = z++, x = y;
-		while (isect(y, z)) z = erase(z);
-		if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
-		while ((y = x) != begin() && (--x)->p >= y->p)
+		while(isect(y, z)) z = erase(z);
+		if(x!=begin() && isect(--x, y))
+			isect(x, y = erase(y));
+		while((y=x) != begin() && (--x)->p >= y->p)
 			isect(x, erase(y));
 	}
-	ll query(ll x) {
-		assert(!empty());
+	ll query(ll x){
+		if(empty()) return Max ? 0 : inf;
 		auto l = *lower_bound(x);
-		return l.k * x + l.m;
+		return (l.k * x + l.m) * (Max ? 1 : -1);
 	}
 };
