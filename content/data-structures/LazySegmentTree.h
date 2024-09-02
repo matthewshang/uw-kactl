ca2aa8b43c31f985bca4cfa9069bdd2a3dd31e8d
Evirir <gojunxing@gmail.com>
2024-09-02 16:08:49 +0800

    Add Evirir's lazy segtree

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -1,66 +1,73 @@
 /**
- * Author: Simon Lindholm
- * Date: 2016-10-08
+ * Author: Evirir
+ * Date: 2024-09-02
  * License: CC0
  * Source: me
- * Description: Segment tree with ability to add or set values of large intervals, and compute max of intervals.
- * Can be changed to other things.
- * Use with a bump allocator for better performance, and SmallPtr or implicit indices to save memory.
+ * Description: Segment tree
  * Time: O(\log N).
- * Usage: Node* tr = new Node(v, 0, sz(v));
  * Status: stress-tested a bit
  */
 #pragma once
 
-#include "../various/BumpAllocator.h"
+class LazySegmentTree {
+private:
+    static constexpr ll kDef = 0;
+    static constexpr ll kDum = 0;
+    static constexpr ll kDefLazy = 0;
 
-const int inf = 1e9;
-struct Node {
-	Node *l = 0, *r = 0;
-	int lo, hi, mset = inf, madd = 0, val = -inf;
-	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
-	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
-		if (lo + 1 < hi) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
-			val = max(l->val, r->val);
-		}
-		else val = v[lo];
-	}
-	int query(int L, int R) {
-		if (R <= lo || hi <= L) return -inf;
-		if (L <= lo && hi <= R) return val;
-		push();
-		return max(l->query(L, R), r->query(L, R));
-	}
-	void set(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) mset = val = x, madd = 0;
-		else {
-			push(), l->set(L, R, x), r->set(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void add(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) {
-			if (mset != inf) mset += x;
-			else madd += x;
-			val += x;
-		}
-		else {
-			push(), l->add(L, R, x), r->add(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void push() {
-		if (!l) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(lo, mid); r = new Node(mid, hi);
-		}
-		if (mset != inf)
-			l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
-		else if (madd)
-			l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
-	}
+    int size_;
+    vector<ll> v, lazy;
+
+    void update(int s, int e, ll val, int k, int l, int r) {
+        if (r < s || e < l) return;
+        if (s <= l && r <= e) {
+            lazy[k] += val;
+            apply(k, l, r, val);
+            return;
+        }
+        push(k, l, r);
+        update(s, e, val, k * 2, l, (l + r) >> 1);
+        update(s, e, val, k * 2 + 1, ((l + r) >> 1) + 1, r);
+        v[k] = merge(v[k * 2], v[k * 2 + 1]);
+    }
+
+    ll query(int s, int e, int k, int l, int r) {
+        if (r < s || e < l) return kDum;
+        if (s <= l && r <= e) return v[k];
+        push(k, l, r);
+        ll lc = query(s, e, k * 2, l, (l + r) >> 1);
+        ll rc = query(s, e, k * 2 + 1, ((l + r) >> 1) + 1, r);
+        return merge(lc, rc);
+    }
+
+public:
+    LazySegmentTree() : v{}, lazy{} {}
+    LazySegmentTree(int n) {
+        for (size_ = 1; size_ < n;) size_ <<= 1;
+        v.resize(size_ * 4, kDef);
+        lazy.resize(size_ * 4, kDefLazy);
+    }
+    inline void push(int k, int l, int r) {
+        if (lazy[k] == kDefLazy) return;
+        if (l != r) {
+            int mid = (l + r) >> 1;
+            lazy[k * 2] += lazy[k];
+            apply(k * 2, l, mid, lazy[k]);
+            lazy[k * 2 + 1] += lazy[k];
+            apply(k * 2 + 1, mid + 1, r, lazy[k]);
+        }
+        lazy[k] = kDefLazy;
+    }
+    void apply(int k, int l, int r, ll val) {
+        v[k] += (r - l + 1) * val;  // remember to consider the range!
+    }
+    inline ll merge(ll x, ll y) {
+        return x + y;
+    }
+    inline void update(int l, int r, ll val) {
+        update(l, r, val, 1, 0, size_ - 1);
+    }
+    inline ll query(int l, int r) {
+        return query(l, r, 1, 0, size_ - 1);
+    }
 };
