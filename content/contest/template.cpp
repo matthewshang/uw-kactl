b7d71c41abe00249b7cba006037c16a65e4edb4d
Evirir <gojunxing@gmail.com>
2024-09-13 00:21:20 +0800

    Update template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define forn(i, a, b) for(int i = a; i < (b); ++i)
+#define fore(i, a, b) for(int i = a; i <= (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
 #define mset(d,val) memset(d,val,sizeof(d))
-typedef long long ll;
-typedef pair<int, int> ii;
-typedef vector<int> vi;
+using ll = long long;
+using ii = pair<int, int>;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
